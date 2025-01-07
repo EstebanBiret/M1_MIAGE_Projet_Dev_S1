454a90f5b972eeb47a104d98f14e5638385a7259
Esteban BIRET-TOSCANO <esteban.biret@gmail.com>
2025-01-07 17:48:19 +0100

    Update Stocker_Magasin.sql

--- sql/Stocker_Magasin.sql
+++ sql/Stocker_Magasin.sql
@@ -1,13 +1,13 @@
 CREATE table magasin(
-    idMagasin int AUTO_INCREMENT PRIMARY KEY,
-    nomMagasin varchar(20) NOT NULL,
-    villeMagasin varchar(20) NOT NULL
+    idMagasin int(11) AUTO_INCREMENT PRIMARY KEY,
+    nomMagasin varchar(128) NOT NULL,
+    adresseMagasin varchar(128) NOT NULL
 );
 
 CREATE table stocker(
-    idMagasin int,
-    idProduit int,
-    quantiteEnStock int NOT NULL,
+    idMagasin int(11),
+    idProduit int(11),
+    quantiteEnStock int(11) NOT NULL,
     PRIMARY KEY(idMagasin,idProduit),
     FOREIGN KEY(idMagasin) REFERENCES magasin(idMagasin),
     FOREIGN KEY(idProduit) REFERENCES produit(idProduit)
