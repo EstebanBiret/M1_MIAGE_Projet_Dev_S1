7237e65ee2919d52b4b5eda3d149f3becda3af2d
Esteban BIRET-TOSCANO <100210730+EstebanBiret@users.noreply.github.com>
2025-01-08 10:11:45 +0100

    update dump.sql

--- sql/dump_mysql.sql
+++ sql/dump_mysql.sql
@@ -14,35 +14,99 @@ USE `projet_dev_m1_miage_s1`;
 -- Structure de la table `client`
 --
 
+CREATE table magasin(
+  `idMagasin` int(11) NOT NULL AUTO_INCREMENT,
+  `nomMagasin` varchar(128) NOT NULL,
+  `adresseMagasin` varchar(128) NOT NULL,
+  PRIMARY KEY(idMagasin)
+);
+
+CREATE TABLE `produit` (
+  `idProduit` int(11) NOT NULL AUTO_INCREMENT,
+  `libelleProduit` varchar(128) NOT NULL,
+  `prixUnitaire` DECIMAL(6,2) NOT NULL,
+  `prixKilo` DECIMAL(6,2) NOT NULL,
+  `nutriscore` char(128) NOT NULL,
+  `poidsProduit` DECIMAL(6,3) NOT NULL,
+  `conditionnementProduit` varchar(128) NOT NULL,
+  `marqueProduit` varchar(128) NOT NULL,
+  PRIMARY KEY (idProduit)
+);
+
+CREATE TABLE `categorie` (
+  `idCategorie` int(11) NOT NULL AUTO_INCREMENT,
+  `nomCategorie` varchar(128) NOT NULL,
+  PRIMARY KEY (idCategorie)
+);
+
+CREATE TABLE `profil` (
+  `idProfil` int(11) NOT NULL AUTO_INCREMENT,
+  `nomProfil` varchar(128) NOT NULL,
+  PRIMARY KEY (idProfil)
+);
+
+CREATE TABLE `commande` (
+  `idCommande` int(11) NOT NULL AUTO_INCREMENT,
+  `statutCommande` varchar(128) NOT NULL,
+  `dateCommande` date NOT NULL,
+  `heureCommande` time NOT NULL,
+  PRIMARY KEY (idCommande)
+);
+
 CREATE TABLE `client` (
   `idClient` int(11) NOT NULL AUTO_INCREMENT,
+  `idMagasin` int(11) NOT NULL,
   `nomClient` varchar(128) NOT NULL,
   `adresseClient` varchar(128) NOT NULL,
   `telClient` varchar(128) NOT NULL,
-   PRIMARY KEY (idClient)
+  PRIMARY KEY(idClient),
+  FOREIGN KEY(idMagasin) REFERENCES magasin(idMagasin)
+);
+
+CREATE TABLE `client_profil` (
+  `idClient` int(11) NOT NULL,
+  `idProfil` int(11) NOT NULL,
+  PRIMARY KEY(idClient, idProfil),
+  FOREIGN KEY(idClient) REFERENCES client(idClient),
+  FOREIGN KEY(idProfil) REFERENCES profil(idProfil)
+);
+
+CREATE TABLE `appartenir` (
+  `idCategorie` int(11) NOT NULL,
+  `idProduit` int(11) NOT NULL,
+  PRIMARY KEY (idCategorie, idProduit),
+  FOREIGN KEY(idCategorie) REFERENCES categorie(idCategorie),
+  FOREIGN KEY(idProduit) REFERENCES produit(idProduit)
+);
+
+CREATE table `stocker` (
+  `idMagasin` int(11) NOT NULL,
+  `idProduit` int(11) NOT NULL, 
+  `quantiteEnStock` int(11) NOT NULL,
+  PRIMARY KEY(idMagasin,idProduit),
+  FOREIGN KEY(idMagasin) REFERENCES magasin(idMagasin),
+  FOREIGN KEY(idProduit) REFERENCES produit(idProduit)
+);
+
+CREATE TABLE `panier` (
+  `idPanier` int(11) NOT NULL AUTO_INCREMENT,
+  `idClient` int(11) NOT NULL,
+  PRIMARY KEY (idPanier),
+  FOREIGN KEY (idClient) REFERENCES client(idClient)
+);
+
+CREATE TABLE `panier_produit` (
+  `idPanier` int(11) NOT NULL,
+  `idProduit` int(11) NOT NULL,
+  `quantiteVoulue` int(11) NOT NULL,
+  `modeLivraison` varchar(128) NOT NULL,
+  PRIMARY KEY (idPanier, idProduit),
+  FOREIGN KEY (idPanier) REFERENCES panier(idPanier),
+  FOREIGN KEY (idProduit) REFERENCES produit(idProduit)
 );
 
 --
 -- Insertion des données dans la table `client`
 --
-
-INSERT INTO `client` (`nomClient`, `adresseClient`, `telClient`) VALUES
-('Test', '1 rue mysql', '0654452112');
-
--- --------------------------------------------------------
-
--- Index pour la table `custom_location`
---
--- ALTER TABLE `custom_location`
---  ADD PRIMARY KEY (`id`),
---  ADD KEY `custom_location_fk_id_nar` (`id_narrative`),
---  ADD KEY `fk_custom_location_country` (`id_country`);
-
---
--- Contraintes pour la table `custom_location`
---
--- ALTER TABLE `custom_location`
---  ADD CONSTRAINT `custom_location_fk_id_nar` FOREIGN KEY (`id_narrative`) REFERENCES `narrative` (`id`),
---  ADD CONSTRAINT `fk_custom_location_country` FOREIGN KEY (`id_country`) REFERENCES `country` (`id`);
 
 COMMIT;
